7688fb14839db5d9882f848048330f6ca0a2af59
julie-mac <123594716+julie-mac@users.noreply.github.com>
2023-03-30 21:08:55 -0400

    finished question bank for quiz

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
+//Setting up various variables necessary to our quiz
 const startButton = document.getElementById("start");
 const time = 60;
 let timeLeft = time;
 let score = 0;
 let currentQuestion = 0;
 
-
-const questions = [  {    
+//Storing the questions and answers in objects within an array
+const questions = [{    
         question: "How do you get an HTML element to call a function when the user clicks it?",
         choices: ["someElement.addClickEvent(function)", 
         "someElement.addEventListener('click', someFunction)", 
@@ -22,14 +23,44 @@ const questions = [  {
         answer: "var someVar = 'some value'"
     },
     {
-        question: "Which planet rules over communication in astrology?",
-        choices: ["Mars", "Mercury", "Venus", "Neptune"],
-        answer: "Mercury"
+        question: "How would you dynamically create an element using Javascript?",
+        choices: ["document.createElement(<new element>)", 
+        "document.newEl('new element')", 
+        "document.createElement('new element')", 
+        "createElement('new element')"],
+        answer: "document.createElement('new element')"
     },
     {
-        question: "How many different branches of astrology exist?",
-        choices: ["5", "10", "15", "80+"],
-        answer: "80+"
+        question: "How would you then append that new element as the child of a target element?",
+        choices: ["appendChild(target, new-element)", 
+        "target.appendChild(new-element)", 
+        "target.append(new-element)", 
+        "target --> append(new-element)"],
+        answer: "target.appendChild(new-element)"
+    },
+    {
+        question: "How would you call the value of the FIRST item in an array?",
+        choices: ["array(1)", 
+        "array[1]", 
+        "array[0]", 
+        "array.0"],
+        answer: "array[0]"
+    },
+    {
+        question: "How would you log 'Hello World!' to the console?",
+        choices: ["print('Hello World!)", 
+        "log('Hello World!')", 
+        "console.log(Hello World!)", 
+        "console.log('Hello World!')"],
+        answer: "console.log('Hello World!')"
+    },
+    {
+        question: "How would you call the function someFunction?",
+        choices: ["run someFunction()", 
+        "someFunction()", 
+        "someFunction", 
+        "run(someFunction)"],
+        answer: "someFunction()"
     }]
 
 startButton.addEventListener("click", runQuiz);
@@ -44,7 +75,7 @@ function displayQuestion() {
     const choiceContainer = document.createElement("div");
     choiceContainer.setAttribute("id", "choice-container");
     for (i=0 ; i<questions[currentQuestion].choices.length ; i++) {
-        const choice = document.createElement("p");
+        const choice = document.createElement("button");
         choice.textContent = questions[currentQuestion].choices[i];
         choice.setAttribute("class", "choices")
         choiceContainer.appendChild(choice);
@@ -54,8 +85,14 @@ function displayQuestion() {
             score++;
             var display = document.createElement("p");
             display.textContent = "Correct! Good job!";
-
-        }})
+            questionContainer.appendChild(display);
+        } else if (choice.textContent !== questions[currentQuestion].answer) {
+            var display = document.createElement("p");
+            display.textContent = "Incorrect. 10 seconds has been deducted from the timer.";
+            timeLeft = timeLeft - 10;
+            questionContainer.appendChild(display);
+        }
+    })
     }
     questionContainer.appendChild(choiceContainer);
 
